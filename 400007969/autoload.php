c394715aafcb40c9459752c36194686030a89c2d
Aaron Harris <aaronah02@icloud.com>
2023-11-28 21:36:40 -0400

    Added and fixed autloader issues

---
+++ 400007969/autoload.php
@@ -0,0 +1,24 @@
+<?php
+
+spl_autoload_register(function ($className) {
+	// Get the filepath of the class.
+	$filepath = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+
+  // echo $filepath . '<br>';
+
+
+	// Remove the namespace from the filepath.
+	if (strpos($className, 'App\Core\\') === 0) {
+		$filepath = str_replace('App' . DIRECTORY_SEPARATOR, '', $filepath);
+  }
+
+  // echo $filepath . '<br>';
+
+	// If the file exists, require it.
+	if (file_exists($filepath)) {
+		require_once $filepath;
+	} else {
+		trigger_error('The class `' . $className . '` does not exist.', E_USER_ERROR);
+    exit();
+	}
+});
